7f3c4a8e30fa29e54902645e41306099badcf44b
taslimaakter <22101186@uap-bd.edu>
2024-11-29 14:09:29 +0600

    settings

--- hub/models.py
+++ hub/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event_s(models.Model):
+    title = models.CharField(max_length=200, default="Untitled Event")
+    image = models.ImageField(upload_to='event_images/', blank=True, null=True)
+    date_start = models.DateField(null=True)
+    date_end = models.DateField(null=True)
+    time = models.CharField(max_length=100,null=True)
+    venue = models.CharField(max_length=255,null=True)
+    entry_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.0,null=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
