63ffb67ce6327667c35a06c4f8234e18d95c1a13
taslimaakter <22101186@uap-bd.edu>
2025-06-17 23:20:43 +0600

    updated

--- artist_showcase/settings.py
+++ artist_showcase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b$odky05!u)2t@xh8=x(d+t@ekr$oheb%%byox)no^urvl-e5e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',  'localhost','art_hub01.onrender.com']
+ALLOWED_HOSTS = ['art-hub01-1.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
